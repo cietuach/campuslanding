c1d319780469c37e7574e61d4d1c949b3fb1b23e
Bart Solo <yosoybartsolo@gmail.com>
2023-05-16 13:34:05 -0600

    Desktop page finished

--- pages/desktop.js
+++ pages/desktop.js
@@ -1,23 +1,37 @@
-//a page that shows the logos of the appstore and google play store
-
-import React from "react";
-
-export default function Download() {
+export default function Desktop() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-7xl py-12 px- sm:px-0 lg:px-0 lg:py-24 sm:items-center">
-        <div className="space-y-12">
-          <div className="place-items-center space-y-4 lg:space-y-1 lg:scale-75 lg:grid lg:grid-cols-4"></div>
-          <div className="flex justify-center items-center h-30">
-            <div className="max-w-7xl">
-              <div className="aspect-w-16 aspect-h-9 hidden sm:block">
-                <img
-                  className="object-contain h-96 w-full"
-                  src="/images/Download.png"
-                  alt="Download"
-                />
-              </div>
+    <div className="bg-purple-900">
+      <div className="max-w-7xl mx-auto py-16 px-4 h-screen sm:px-6 lg:px-8">
+        <div className="bg-zinc-100 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
+          <div className="pt-10 pb-12 px-6 sm:pt-16 sm:px-10 lg:p-12 xl:p-16">
+            <div className="lg:self-center">
+              <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+                <span className="block text-purple-900">
+                  La app Campus Seguro
+                </span>
+                <span className="block text-purple-900">
+                  Funciona solo en dispositivos móviles.
+                </span>
+              </h2>
+              <p className="mt-4 text-lg leading-6 text-purple-500">
+                Accede desde tu dispositivo móvil para descargar la app.
+              </p>
+              <a href="/">
+                <button
+                  className="bg-purple-900 rounded-md w-auto px-3 py-1 mt-4 text-base text-white font-semibold"
+                  href="/"
+                >
+                  Regresar
+                </button>
+              </a>
             </div>
+          </div>
+          <div className="-mt-6 ">
+            <img
+              className="mt-10"
+              src="https://res.cloudinary.com/djlx5smmq/image/upload/v1683660688/logo-CampusSeguro-nuevo_kfj2bk.png"
+              alt="Campus Seguro"
+            />
           </div>
         </div>
       </div>
