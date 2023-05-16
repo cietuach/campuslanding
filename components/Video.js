17e021c53ca665cb81b44cac0f74c93753fb83c0
Bart Solo <yosoybartsolo@gmail.com>
2023-05-16 13:59:51 -0600

    ver video button

--- components/Video.js
+++ components/Video.js
@@ -13,6 +13,14 @@ export default function Video({ id }) {
             }}
           />
         </div>
+        <div className="mt-10 flex items-center gap-x-6">
+          <a
+            href="https://www.youtube.com/watch?v=HwDCBP9Id40"
+            className="rounded-md bg-purple-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Ver video
+          </a>
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Example() {
         <div className="space-y-12">
           <div className="place-items-center space-y-4 lg:space-y-1 lg:scale-75 lg:grid lg:grid-cols-4"></div>
           <Hero />
-          <Video id="dQw4w9WgXcQ" />
+          <Video id="HwDCBP9Id40" />
           <Features />
           <Devs />
           <Faqs />
