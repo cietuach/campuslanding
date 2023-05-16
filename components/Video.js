7bfbdf891e2250748464eec333a2bac7d7a427b8
Bart Solo <yosoybartsolo@gmail.com>
2023-05-16 13:06:14 -0600

    Youtube video hidden on mobile

--- components/Video.js
+++ components/Video.js
@@ -4,7 +4,7 @@ export default function Video({ id }) {
   return (
     <div className="flex justify-center items-center h-30">
       <div className="max-w-7xl">
-        <div className="aspect-w-16 aspect-h-9">
+        <div className="aspect-w-16 aspect-h-9 hidden sm:block">
           <YouTube
             videoId={id}
             opts={{
