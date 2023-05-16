b55f11a1dbf0f2a326041cc192ee1f3021d6b749
Bart Solo <yosoybartsolo@gmail.com>
2023-05-16 12:59:54 -0600

    YouTube Video Embed

---
+++ components/Video.js
@@ -0,0 +1,19 @@
+import YouTube from "react-youtube";
+
+export default function Video({ id }) {
+  return (
+    <div className="flex justify-center items-center h-30">
+      <div className="max-w-7xl">
+        <div className="aspect-w-16 aspect-h-9">
+          <YouTube
+            videoId={id}
+            opts={{
+              width: "1000",
+              height: "500",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Features from "../components/Features";
 import Faqs from "../components/Faqs";
 import Footer from "../components/Footer";
 import Devs from "../components/Devs";
+import Video from "../components/Video";
 
 export default function Example() {
   return (
@@ -12,6 +13,7 @@ export default function Example() {
         <div className="space-y-12">
           <div className="place-items-center space-y-4 lg:space-y-1 lg:scale-75 lg:grid lg:grid-cols-4"></div>
           <Hero />
+          <Video id="dQw4w9WgXcQ" />
           <Features />
           <Devs />
           <Faqs />
